afa2bf9afdfe2197c644e415b0e6b459deb4e023
Freddy <efredreyes9@gmail.com>
2018-03-08 16:09:07 -0600

    Alumnos

--- SGA/Presentacion/exportarArchivos.cs
+++ SGA/Presentacion/exportarArchivos.cs
@@ -3,9 +3,13 @@ using System.Windows.Forms;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraGrid.Export;
 using DevExpress.XtraGrid;
+using DevExpress.XtraEditors;
+using System.IO;
+using System.Drawing.Imaging;
+
 namespace Presentacion
 {
-    public  class exportarArchivos
+    public class exportarArchivos
     {
         public void ExportarExcel(GridControl g, string Titulo)
         {
@@ -15,7 +19,7 @@ namespace Presentacion
                 o.Filter = "Libro Excel|*.xlsx";
                 o.ShowDialog();
                 g.ExportToXlsx(o.FileName);
-                MessageBox.Show(Titulo + " " + "exportados con exito", "SGA", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+                MessageBox.Show(Titulo + " " + "exportados con exito", "SGA", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
@@ -24,7 +28,7 @@ namespace Presentacion
             }
         }
 
-        public void ExportarPDF( GridControl g, string Titulo)
+        public void ExportarPDF(GridControl g, string Titulo)
         {
             try
             {
@@ -40,6 +44,21 @@ namespace Presentacion
                 throw ex;
             }
         }
+
+        //public void ConvertirImagen(PictureEdit picture )
+        //{
+        //    try
+        //    {
+        //        MemoryStream ms = new MemoryStream();
+        //        picture.Image.Save(ms, ImageFormat.Png);
+        //        byte[] arregloFoto = ms.GetBuffer();
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw ex;
+        //    }
+        //}
 
     }
 }
